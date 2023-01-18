08ac6cb11db1ccc8c92e4867e2440b51b4a2a777
tori6969 <0675dgwgjp@gmail.com>
2023-01-18 23:29:09 +0900

    アトリエ予約表示ページサンプル

--- functions.php
+++ functions.php
@@ -244,5 +244,6 @@ require get_template_directory() . '/self-made-php/showWorkshopReserve.php';
 require get_template_directory() . '/self-made-php/registerWorkshopReserve.php';
 require get_template_directory() . '/self-made-php/deleteWorkshopReserve.php';
 require get_template_directory() . '/self-made-php/showStdioReserve.php';
+require get_template_directory() . '/self-made-php/registerStdioReserve.php';
 //require get_template_directory() . '/self-made-php/registerWorkshopReserveDb.php';
 require get_template_directory() . '/self-made-php/showExhibition.php';
