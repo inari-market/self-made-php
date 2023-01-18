fc2a8982ca1d3beb52b86a93c5f3b57e2bb954a1
tori6969 <0675dgwgjp@gmail.com>
2023-01-18 14:31:08 +0900

    functions.php

--- functions.php
+++ functions.php
@@ -222,7 +222,7 @@ function init_session_start(){
 add_action('init', 'init_session_start');
 
 
-?/*
+/*
 require get_template_directory() . '/self-made-php/regist_exhibition.php';
 require get_template_directory() . '/self-made-php/exhibitions.php';
 require get_template_directory() . '/self-made-php/regist_author.php';
