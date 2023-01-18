dd9587ca2743ec6691f1ec55ecec0bab4d173023
tori6969 <0675dgwgjp@gmail.com>
2023-01-18 14:36:23 +0900

    functions.php

--- self-made-php/exhibitions.php
+++ self-made-php/exhibitions.php
@@ -5,7 +5,7 @@
         
         if( is_page( 'exhibitions' )) {//特定の固定ページの時だけ処理させる 
 
-            include_once dirname( __FILE__ ).'/db.php';
+            include_once dirname( __FILE__ ).'/../db.php';
             
             try {
                 // データベースに接続します。
