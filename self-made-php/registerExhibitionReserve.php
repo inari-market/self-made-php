10aa0b2520bbbe0e251c8512c85e1bcd251e0c93
tori6969 <0675dgwgjp@gmail.com>
2023-01-23 18:11:44 +0900

    アトリエ予約表示ページサンプル

--- self-made-php/registerExhibitionReserve.php
+++ self-made-php/registerExhibitionReserve.php
@@ -2,7 +2,7 @@
 //実装時はコメント解除
 //いったん放置
 function register_exhibition_reserve($content) {
-  if( is_page( 'stdio_reserves/new' ))  //固定ページ「sample_cal」の時だけ処理させる
+  if( is_page( 'exhibition_reserves/new' ))  //固定ページ「sample_cal」の時だけ処理させる
   {
 
 
