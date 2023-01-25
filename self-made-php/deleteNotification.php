688ab3582ba230c9fce50e1c922a408d6d52c347
masa6369 <f.masaya0609@icloud.com>
2023-01-25 22:20:33 +0900

    お知らせ情報削除ページ

--- self-made-php/deleteNotification.php
+++ self-made-php/deleteNotification.php
@@ -1,35 +1,8 @@
 <?php
 //実装時はコメント解除
-
 function delete_notification($content) {
  if( is_page( 'notification/delete' ))  //固定ページ「sample_cal」の時だけ処理させる
  {
-
-?>
-
-<?php
-$id = $_GET['id'];
-if (! empty($id)) {
-
-    try {
-        include_once dirname( __FILE__ ).'/../db.php';
-        $dbh = DbUtil::Connect();
-        $sql = 'DELETE FROM notice where notice = :id';
-        $stmt = $dbh->prepare( $sql );
-        $stmt->bindValue( ':id', $id, PDO::PARAM_INT );
-        // SQL文を実行します。
-        $stmt->execute();
-        session_start();
-        $_SESSION['delete_notification']="削除完了";
-        echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/notification/delete/";</script>';
-        exit();
-
-    }catch( PDOException $e ){
-        echo( '接続失敗: ' . $e->getMessage() . '<br>' );
-        exit();
-    }
-}
-
 
 ?>
 
@@ -51,7 +24,7 @@ if (! empty($id)) {
     </head>
     <body>
         <div class='c'>
-    <h1>お知らせ情報</h1>
+    <h1>お知らせ情報削除ページ</h1>
 
                 <table width="90%" class ='c'>
                 <tr>
@@ -68,7 +41,7 @@ if (! empty($id)) {
             // SQL文を用意します。
             // :で始まる部分が後から値がセットされるプレースホルダです。
             // 複数回SQL文を実行する必要がある場合はここからexecute()までを>繰り返します。
-            $sql = 'SELECT * FROM notice order by start asc';
+            $sql = 'SELECT * FROM notice';
             // SQL文を実行する準備をします。
             $stmt = $dbh->prepare( $sql );
             // SQL文を実行します。
@@ -80,11 +53,17 @@ if (! empty($id)) {
             <td><?php echo $row['notice_id']; ?></td>
             <td><?php echo $row['title']; ?></td>
             <td><?php echo $row['body']; ?></td>
-           <td><a href="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>?id=<?php echo $row['notice_id']; ?>">削除</a></td>
+            <td>
+            <?php
+        ?>
+            </td>
+            <td><a href="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>?id=<?php echo $row['notice_id']; ?>">削除</a></td>
+
         </tr>
     <?php } ?>
 
                 </table>
+
                 <?php
             session_start();
             if(! empty($_SESSION['delete_notification'])){
@@ -94,10 +73,41 @@ if (! empty($id)) {
                 echo("<br><br>");
             }
             ?>
+
 </div>
     </body>
 </html>
 
+<?php
+$id = $_GET['id'];
+if (! empty($id)) {
+
+    try {
+        include_once dirname( __FILE__ ).'/../db.php';
+        // SQL文を用意します。
+        // :で始まる部分が後から値がセットされるプレースホルダです。
+        // 複数回SQL文を実行する必要がある場合はここからexecute()までを繰り返し ます。
+        $dbh = DbUtil::Connect();
+        $sql = 'DELETE FROM notice where notice_id = :id';
+        // SQL文を実行する準備をします。
+        $stmt = $dbh->prepare( $sql );
+        // プレースホルダに実際の値をバインドします。
+        //   ->bindValue( プレースホルダ名, バインドする値, データの型 )
+        $stmt->bindValue( ':id', $id, PDO::PARAM_INT );
+        // SQL文を実行します。
+        $stmt->execute();
+        session_start();
+        $_SESSION['delete_notification']="削除完了";
+        echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/notification/delete/";</script>';
+        exit();
+
+    }catch( PDOException $e ){
+        echo( '接続失敗: ' . $e->getMessage() . '<br>' );
+        exit();
+    }
+
+}
+?>
 
 <?php
 
