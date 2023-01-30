2ccbddeb2a5b9875b6a433190882d3350282be27
dekopon21020014 <renueta@icloud.com>
2023-01-30 20:11:29 +0900

    バインドする順番を間違っていました．

--- self-made-php/notification/show.php
+++ self-made-php/notification/show.php
@@ -5,16 +5,11 @@
             include_once dirname( __FILE__ ).'/../../db.php';
             $dbh = DbUtil::Connect(); // データベースに接続します。
             $sql = 'SELECT * FROM notice where id=:id'; 
-            $stmt->bindValue( ':id', $_GET['id'], PDO::PARAM_STR );
             $stmt = $dbh->prepare( $sql ); 
+            $stmt->bindValue( ':id', $_GET['id'], PDO::PARAM_STR );
             $stmt->execute(); // sqlの実行
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-?>  
-            <ul>
-                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?> 
-                    <li><a href="notifications/show/?id=<?php echo $row['id']?>"> <?php echo $row['title']?></a></li>
-                <?php } ?>
-            <ul>				
+?>  				
             <h3><?php echo $row['title']?></h3>
             <p><?php echo $row['body'] ?></p>
 <?php
