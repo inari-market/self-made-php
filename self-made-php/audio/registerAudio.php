01118467607e5b05cf6626a15ded2c5240ad0575
masa6369 <f.masaya0609@icloud.com>
2023-01-30 18:00:17 +0900

    音声情報

--- self-made-php/audio/registerAudio.php
+++ self-made-php/audio/registerAudio.php
@@ -60,56 +60,44 @@ if(isset($_POST['submit'])){
 
         $_SESSION['register_audio'] = '';
 
-        if( empty($_POST["filename"])){
-            $_SESSION["register_audio"]="ファイル名を指定してください 。";
-            echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/audio/new/";</script>';
-            exit();
-          }
-        if(filesize($_FILES["upfile"]["tmp_name"] > 3000000)){
-            $_SESSION["register_audio"]="ファイルサイズが大きいです。";
-            echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/audio/new/";</script>';
-            exit();
-          }
-
-          if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-              if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "/var/www/html/audio/ ". $audio)) {
-                  // SQL文を用意します。
-                  // :で始まる部分が後から値がセットされるプレースホルダです。
-                  // 複数回SQL文を実行する必要がある場合はここからexecute()までを 繰り返します。
-                  try{
-
-                      $_SESSION['register_audio']="登録完了";
-
-                      $dbh = DbUtil::Connect();
-                      $sql = 'INSERT INTO audio1 (filename) VALUES(:filename)';
-                      // SQL文を実行する準備をします。
-                      $stmt = $dbh->prepare( $sql );
-                      // プレースホルダに実際の値をバインドします。
-                      //   ->bindValue( プレースホルダ名, バインドする値, データの型 )
-                      $stmt->bindValue( ':filename', $inputName, PDO::PARAM_STR );
-                      // SQL文を実行します。
-                      $stmt->execute();
-
-                      $_SESSION['register_audio']="登録完了";
-
-                      unset($inputName);
-                      unset($_POST['filename']);
-
-                      chmod("/var/www/html/audio " . $audio, 0644);
-
-                  }catch( PDOException $e ){
-                      echo( '接続失敗: ' . $e->getMessage() . '<br>' );
-                      exit();
-                  }
+        if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
+          if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "/var/www/html/audio/ ". $audio)) {
+              // SQL文を用意します。
+              // :で始まる部分が後から値がセットされるプレースホルダです。
+              // 複数回SQL文を実行する必要がある場合はここからexecute()までを 繰り返します。
+              try{
+                  $dbh = DbUtil::Connect();
+                  $sql = 'INSERT INTO audio1 (filename) VALUES(:filename)';
+                  // SQL文を実行する準備をします。
+                  $stmt = $dbh->prepare( $sql );
+                  // プレースホルダに実際の値をバインドします。
+                  //   ->bindValue( プレースホルダ名, バインドする値, データの型 )
+                  $stmt->bindValue( ':filename', $inputName, PDO::PARAM_STR );
+                  // SQL文を実行します。
+                  $stmt->execute();
+              }catch( PDOException $e ){
+                  echo( '接続失敗: ' . $e->getMessage() . '<br>' );
+                    exit();
               }
-          }
-    } else {
-        $_SESSION["register_audio"]= "ファイルをアップロードできません。";
-    }
-    echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/audio/new/";</script>';
-    exit();
-}
+              unset($inputName);
+              unset($_POST['filename']);
 
+              chmod("/var/www/html/audio " . $audio, 0644);
+              $_SESSION["register_audio"]= $audio . "をアップロードしました。";
+              echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/audio/new/";</script>';
+              exit();
+            } else {
+              $_SESSION["register_audio"]= "ファイルをアップロードできません。";
+              echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/audio/new/";</script>';
+              exit();
+          }
+        } else {
+            $_SESSION["register_audio"]= "ファイルが選択されていません。";
+            echo '<script type="text/javascript">window.location.href = window.location.hreg = "http://18.209.25.203/audio/new/";</script>';
+            exit();
+      }
+  }
+}
 ?>
 
 <?php
