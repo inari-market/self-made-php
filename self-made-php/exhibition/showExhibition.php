21fe3cded9dbccb41dcae7ea398830bb0b64185f
tori6969 <0675dgwgjp@gmail.com>
2023-01-31 17:23:41 +0900

    functions.phpの変更

--- self-made-php/exhibition/showExhibition.php
+++ self-made-php/exhibition/showExhibition.php
@@ -23,18 +23,7 @@ function show_exhibition($content) {
     </head>
     <body>
         <div class='c'>
-    <h1>企画展情報表示</h1>
-
-                <table width="90%" class ='c'>
-                <tr>
-                  <th>企画展ID</th>
-                  <th>企画展名</th>
-                  <th>開始日</th>
-                  <th>終了日</th>
-                  <th>主催者名</th>
-                  <th>概要</th>
-                  <th>写真</th>
-                  </tr>
+    <h1>展示会情報表示</h1>
 
         <?php
         include_once dirname( __FILE__ ).'/../../db.php';
@@ -52,23 +41,23 @@ function show_exhibition($content) {
             ?>
 
         <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-        <tr>
-            <td><?php echo $row['exhibition_id']; ?></td>
-            <td><?php echo $row['exhibition_name']; ?></td>
-            <td><?php echo $row['start']; ?></td>
-            <td><?php echo $row['end']; ?></td>
-            <td><?php echo $row['organizer']; ?></td>
-            <td><?php echo htmlspecialchars($row['introduction'], ENT_QUOTES, 'UTF-8'); ?></td>
-            <td>
-            <?php
-                $img_url = "http://100.24.172.143/exhibition/";
-                echo '<figure class="wp-block-image size-full is-resized"><img decoding="async" src="' . $img_url . $row['photo_name'] .".png" . '" alt="画像が読み込めませんでした" width="60" height="60"></figure>';
-            ?>
-            </td>
-        </tr>
-    <?php } ?>
+        <div class="is-layout-flex wp-container-9 wp-block-columns">
+            <div class="is-layout-flow wp-block-column">
+                <?php
+                    $img_url = "http://100.24.172.143/exhibition/";
+                    echo '<figure class="wp-block-image size-large is-resized"><img decoding="async"  loading="lazy" src="' . $img_url . $row['photo_name'] .".png" . '" alt="画像が読み込めませんでした" width="60" height="60"></figure>';
+                ?>
+            </div>
 
-                </table>
+            <div class="is-layout-flow wp-block-column">
+                <h2><?php echo $row['exhibition_name']; ?></h2>
+                <p>展示開催日:<?php echo $row['start']."～".$row['end']; ?></p>
+                <p>主催者:<?php echo $row['organizer']; ?></p>
+                <p>概要:<?php echo $row['introduction']; ?></p>
+            </div>
+        </div>
+
+    <?php } ?>
 
 </div>
     </body>
