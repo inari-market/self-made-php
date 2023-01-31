e61c7ae27a43b46323624a78964d9e08db3b996d
dekopon21020014 <renueta@icloud.com>
2023-02-01 04:44:38 +0900

    debug

--- functions.php
+++ functions.php
@@ -252,3 +252,4 @@ require get_template_directory() . '/self-made-php/notification/show.php';
 require get_template_directory() . '/self-made-php/notification/new.php';
 require get_template_directory() . '/self-made-php/notification/deleteNotification.php';
 require get_template_directory() . '/self-made-php/notification/index.php';
+require get_template_directory() . '/self-made-php/work/new.php';
