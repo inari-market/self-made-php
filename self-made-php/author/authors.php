6d59f63831037c52a93f3d4ee7a66a487c78fea3
dekopon21020014 <renueta@icloud.com>
2023-02-01 03:09:08 +0900

    作者一覧をUI班に合わせた

--- self-made-php/author/authors.php
+++ self-made-php/author/authors.php
@@ -17,39 +17,23 @@ hoge
                 // echo '<div class="wp-block-columns">&nbsp;</div>'; // この行の意味はよくわかってない．調べたけどいらなさそう
 
                 // イテレータとかの準備
-                $count   = count( $authors ); // 作者の総数
-                $row     = floor($count / 4 ) + 1; // 1行に4人ずつ表示したときに何行必要か
-                $col     = $count % 4; // 最終行には何人表示されるか
                 $img_url = 'http://52.54.93.120/img/author/'; // 画像の参照先
                 $index   = 0; // 配列のインデックス
 
-                // 詳細・修正・削除ボタンのCSS
-                $css = 'display       : inline-block;  
-                        border-radius : 5%;
-                        font-size     : 9pt;
-                        text-align    : center;
-                        cursor        : pointer;
-                        padding       : 12px 12px;
-                        background    : #ffffff;
-                        color         : rgba(26, 26, 255, 0.68);
-                        line-height   : 1em;
-                        transition    : .3s;
-                        box-shadow    : 6px 6px 3px #666666;
-                        border        : 2px solid #ffffff;';
         
                 // 一行に４人表示する．(横並びに４人表示する)
-                for( $i = 0; $i < $row; $i++ ) { 
+                for( $i = 0; $i < count($authors); $i++ ) { 
                     ?>
 
                     <div class="is-layout-flow wp-block-column" style="flex-basis:33.33%"><div class="wp-block-image">
                     <figure class="aligncenter size-full is-resized"><img decoding="async" src="http://ec2-52-54-93-120.compute-1.amazonaws.com/wp-content/uploads/2023/01/1000001162.png" alt="" class="wp-image-1023" width="200" height="240"></figure></div></div>
 
                     <div class="is-layout-flow wp-block-column" style="flex-basis:66.66%">
-                    <h3><?php echo $row['name']?> -<?php echo $row['furigana'] ?>-</h3>
+                    <h3><?php echo $authors['name']?> -<?php echo $authors['furigana'] ?>-</h3>
 
-                    <p>生年:<?php echo $row['birthday']?></p>
-                    <p>出身：<?php echo $row['birthplace']?></p><br>
-                    <p>代表作：<?php echo $row['work']?></p>
+                    <p>生年:<?php echo $authors['birthday']?></p>
+                    <p>出身：<?php echo $authors['birthplace']?></p><br>
+                    <p>代表作：<?php echo $authors['work']?></p>
                     <div class="button_line007"><a href="http://52.54.93.120/" data-type="page" data-id="1034">詳しく知る</a></div>
                     </div>
 
