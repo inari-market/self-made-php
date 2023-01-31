eb1bf5844755496a603eac6c24967b431e1d033d
tori6969 <0675dgwgjp@gmail.com>
2023-01-31 17:44:03 +0900

    functions.phpの変更

--- self-made-php/exhibition/showExhibition.php
+++ self-made-php/exhibition/showExhibition.php
@@ -54,6 +54,70 @@ function show_exhibition($content) {
             </div>
         </div>
 
+    <h1>今後開催される展示</h1>
+        <?php
+        include_once dirname( __FILE__ ).'/../../db.php';
+            // データベースに接続します。
+            $dbh = DbUtil::Connect();
+
+            // SQL文を用意します。
+            // :で始まる部分が後から値がセットされるプレースホルダです。
+            // 複数回SQL文を実行する必要がある場合はここからexecute()までを>繰り返します。
+            $sql = 'SELECT * FROM exhibition where end < now()';
+            // SQL文を実行する準備をします。
+            $stmt = $dbh->prepare( $sql );
+            // SQL文を実行します。
+            $stmt->execute();
+            ?>
+
+        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+        <div class="is-layout-flex wp-container-9 wp-block-columns">
+            <div class="is-layout-flow wp-block-column">
+                <?php
+                    $img_url = "http://100.24.172.143/exhibition/";
+                    echo '<figure class="wp-block-image size-large is-resized"><img decoding="async"  loading="lazy" src="' . $img_url . $row['photo_name'] .".png" . '" alt="画像が読み込めませんでした" width="600" height="600"></figure>';
+                ?>
+            </div>
+
+            <div class="is-layout-flow wp-block-column">
+                <h2><?php echo $row['exhibition_name']; ?></h2>
+                <p>展示開催日：<?php echo $row['start']."日から".$row['end']; ?></p>
+                <p>主催者：<?php echo $row['organizer']; ?></p>
+                <p>概要：<?php echo htmlspecialchars($row['introduction'], ENT_QUOTES); ?></p>
+            </div>
+        </div>
+
+    <h1>過去に開催された展示</h1>
+        <?php
+        include_once dirname( __FILE__ ).'/../../db.php';
+            // データベースに接続します。
+            $dbh = DbUtil::Connect();
+
+            // SQL文を用意します。
+            // :で始まる部分が後から値がセットされるプレースホルダです。
+            // 複数回SQL文を実行する必要がある場合はここからexecute()までを>繰り返します。
+            $sql = 'SELECT * FROM exhibition where end > now()';
+            // SQL文を実行する準備をします。
+            $stmt = $dbh->prepare( $sql );
+            // SQL文を実行します。
+            $stmt->execute();
+            ?>
+
+        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+        <div class="is-layout-flex wp-container-9 wp-block-columns">
+            <div class="is-layout-flow wp-block-column">
+                <?php
+                    $img_url = "http://100.24.172.143/exhibition/";
+                    echo '<figure class="wp-block-image size-large is-resized"><img decoding="async"  loading="lazy" src="' . $img_url . $row['photo_name'] .".png" . '" alt="画像が読み込めませんでした" width="60" height="60"></figure>';
+                ?>
+            </div>
+
+            <div class="is-layout-flow wp-block-column">
+                <h2><?php echo $row['exhibition_name']; ?></h2>
+                <p>主催者：<?php echo $row['organizer']; ?></p>
+            </div>
+        </div>
+
     <?php } ?>
     </body>
 </html>
